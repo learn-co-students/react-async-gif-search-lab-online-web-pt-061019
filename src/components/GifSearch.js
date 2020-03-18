6d8119d019c4e1c11823c2068342a4b2746241db
thistonyb <this.tonybaker@gmail.com>
2020-03-18 15:29:27 -0700

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -10,14 +10,20 @@ export default class GifSearch extends Component {
   };
 
   onChange = event => {
-    this.setState();
+    this.setState({
+      query: event.target.value
+    });
   };
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" onChange={this.handleInput}></input>
+          <input
+            type="text"
+            onChange={this.handleInput}
+            value={this.state.query}
+          ></input>
         </form>
       </div>
     );
