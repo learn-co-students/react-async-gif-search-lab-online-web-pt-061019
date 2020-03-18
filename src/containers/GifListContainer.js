46d2e78ad7ffa1c63aa66edab39f56dda0322217
thistonyb <this.tonybaker@gmail.com>
2020-03-18 13:53:05 -0700

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -3,9 +3,21 @@ import GifList from "../components/GifList";
 import GifSearch from "../components/GifSearch";
 
 export default class GifListContainer extends Component {
-  state = {};
+  state = {
+    gifObj: []
+  };
 
-  componentDidMount() {}
+  componentDidMount() {
+    fetch(
+      "https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g"
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          gifObj: data.data
+        });
+      });
+  }
 
   render() {
     return;
